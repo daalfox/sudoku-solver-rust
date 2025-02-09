101d80d6032842ac8c1715b022d745b7cac04002
daalfox <daalfox@gmail.com>
2025-02-09 18:06:38 +0330

    Handle case where there are certain cells

--- src/lib.rs
+++ src/lib.rs
@@ -1,26 +1,58 @@
 #![allow(dead_code)]
 
+use std::collections::HashMap;
+
 type Sudoku = [[u8; 9]; 9];
 type CellAddr = (usize, usize);
 
+struct Assumption {
+    alternatives: Vec<u8>,
+}
+
 fn solve(table: &Sudoku) -> Sudoku {
-    let t = *table;
+    let mut t = *table;
+
+    let mut possibilities = calculate_possibilities(&t);
+
+    // if there is a single option item in `empty_cells_map`, fill cell with that value and
+    // recalculate possibilities for relative cells
+    while !possibilities.is_empty() {
+        match possibilities.iter().find(|cell| cell.1.len() == 1) {
+            Some((addr, p)) => {
+                t[addr.0][addr.1] = p[0];
+            }
+            // guess cells with least amount of possibilities
+            None => {
+                eprintln!("{possibilities:?}");
+                unimplemented!("sudoku with uncertain cells are not supported");
+            }
+        }
+
+        possibilities = calculate_possibilities(&t);
+    }
+
+    t
+}
+
+fn calculate_possibilities(table: &Sudoku) -> HashMap<CellAddr, Vec<u8>> {
+    let mut possibilities = HashMap::new();
+
     for (i, row) in table.iter().enumerate() {
         for (j, cell) in row.iter().enumerate() {
             if *cell != 0 {
                 continue;
             }
 
-            let possibilites = calculate_possibilities(table, (i, j));
+            let possibilites = calculate_possibilities_for_cell(table, (i, j));
 
-            dbg!(possibilites);
+            possibilities.insert((i, j), possibilites);
         }
     }
 
-    t
+    possibilities
 }
 
-fn calculate_possibilities(table: &[[u8; 9]; 9], cell: CellAddr) -> Vec<u8> {
+fn calculate_possibilities_for_cell(table: &[[u8; 9]; 9], cell: CellAddr) -> Vec<u8> {
     let mut possibilities = vec![1, 2, 3, 4, 5, 6, 7, 8, 9];
 
     possibilities.retain(|p| !table[cell.0].contains(p));
@@ -39,8 +71,8 @@ fn calculate_possibilities(table: &[[u8; 9]; 9], cell: CellAddr) -> Vec<u8> {
 }
 
 fn get_subgrid_numbers(table: &Sudoku, cell: CellAddr) -> Vec<u8> {
-    let r = cell.0 / 3;
-    let c = cell.1 / 3;
+    let r = (cell.0 / 3) * 3;
+    let c = (cell.1 / 3) * 3;
 
     vec![
         table[r][c],
@@ -107,10 +139,38 @@ mod test {
     }
 
     #[test]
+    fn acceptance2() {
+        let got = solve(&[
+            [3, 0, 6, 5, 0, 8, 4, 0, 0],
+            [5, 2, 0, 0, 0, 0, 0, 0, 0],
+            [0, 8, 7, 0, 0, 0, 0, 3, 1],
+            [0, 0, 3, 0, 1, 0, 0, 8, 0],
+            [9, 0, 0, 8, 6, 3, 0, 0, 5],
+            [0, 5, 0, 0, 9, 0, 6, 0, 0],
+            [1, 3, 0, 0, 0, 0, 2, 5, 0],
+            [0, 0, 0, 0, 0, 0, 0, 7, 4],
+            [0, 0, 5, 2, 0, 6, 3, 0, 0],
+        ]);
+        let want = [
+            [3, 1, 6, 5, 7, 8, 4, 9, 2],
+            [5, 2, 9, 1, 3, 4, 7, 6, 8],
+            [4, 8, 7, 6, 2, 9, 5, 3, 1],
+            [2, 6, 3, 4, 1, 5, 9, 8, 7],
+            [9, 7, 4, 8, 6, 3, 1, 2, 5],
+            [8, 5, 1, 7, 9, 2, 6, 4, 3],
+            [1, 3, 8, 9, 4, 7, 2, 5, 6],
+            [6, 9, 2, 3, 5, 1, 8, 7, 4],
+            [7, 4, 5, 2, 8, 6, 3, 1, 9],
+        ];
+
+        assert_eq!(got, want)
+    }
+
+    #[test]
     fn calculates_possibilities_removes_row_numbers() {
         let table = get_table();
         let row_index = 0;
-        let got = calculate_possibilities(&table, (row_index, 2));
+        let got = calculate_possibilities_for_cell(&table, (row_index, 2));
         let row_numbers: Vec<u8> = table[row_index].into_iter().filter(|x| *x != 0).collect();
 
         assert!(got.iter().all(|x| !row_numbers.contains(x)))
@@ -120,7 +180,7 @@ mod test {
     fn calculates_possibilities_removes_col_numbers() {
         let table = get_table();
         let col_index = 2;
-        let got = calculate_possibilities(&table, (0, col_index));
+        let got = calculate_possibilities_for_cell(&table, (0, col_index));
         let col_numbers: Vec<u8> = get_col_numbers(&table, col_index);
 
         assert!(got.iter().all(|x| !col_numbers.contains(x)))
@@ -130,7 +190,7 @@ mod test {
     fn calculates_possibilities_removes_subgrid_numbers() {
         let table = get_table();
         let cell_addr = (0, 2);
-        let got = calculate_possibilities(&table, cell_addr);
+        let got = calculate_possibilities_for_cell(&table, cell_addr);
         let col_numbers: Vec<u8> = get_subgrid_numbers(&table, cell_addr);
 
         assert!(got.iter().all(|x| !col_numbers.contains(x)))
